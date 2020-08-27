7ce283ec656e768f78b595a0b9a2613a424b22a4
zouinetami <68541868+zouinetami@users.noreply.github.com>
2020-08-27 10:48:58 +0100

    Initial Commit

---
+++ StibitsDevops/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StibitsDevops
+//
+//  Created by stibits on 8/27/20.
+//  Copyright © 2020 stibits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
